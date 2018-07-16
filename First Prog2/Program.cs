4d65a4dc8dcc066dbf1b85f048cb5883bbf09cf9
m7mdsofan <nash2t@hotmail.com>
2018-07-16 03:28:03 +0200

    My Var
    Thna

---
+++ First Prog2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("Hello World");
+
+            int charMin = Char.MinValue;
+            int charMax = Char.MaxValue;
+
+            char myVar = 'M';
+
+
+            Console.WriteLine("Min Char = {0} Max Char = {1} My Character = {2} ", charMin, charMax, myVar);
+
+            Console.ReadKey();
+        }
+    }
+}
