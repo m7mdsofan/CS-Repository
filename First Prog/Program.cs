f09b259ab3bed33d931d27c2ba906b37cb32467f
m7mdsofan <nash2t@hotmail.com>
2018-07-16 03:46:14 +0200

    String Number
    Number and value

---
+++ First Prog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("Hello World");
+
+            int charMin = Char.MinValue;
+            int charMax = Char.MaxValue;
+
+            char myVar = 'M';
+            int MyVarValue = 'M';
+
+            char MyVarValue2 = Convert.ToChar(MyVarValue);
+
+
+            Console.WriteLine("Min Char = {0} Max Char = {1} My Character = {2} My Variable Number = {3} My Convert Value is {4}", charMin, charMax, myVar, MyVarValue, MyVarValue2);
+        
+            Console.ReadKey();
+        }
+    }
+}
